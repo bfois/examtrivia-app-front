bdb957d33ed55cffe18947e549c6a5c2e57a864d
BraianF <97342723+bfois@users.noreply.github.com>
2023-03-15 22:16:52 -0300

    resultados arreglados

--- src/app/pages/game/resultados/resultados.component.ts
+++ src/app/pages/game/resultados/resultados.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pregunta } from 'src/app/interfaces/Pregunta';
 import { Respuesta } from 'src/app/interfaces/Respuesta';
 import { Temas } from 'src/app/interfaces/Temas';
@@ -16,17 +16,37 @@ export class ResultadosComponent implements OnInit {
   temasSeleccionados: Temas[] = [];
   nombreTemasSeleccionados: string[] = []
   currentUser: any;
+  temasPreguntas:any = {}
+
   constructor(
     private triviaService: TriviaDataService,
     private authenticationService: AuthenticationService) {
 
   }
   ngOnInit(): void {
+    //OBTIENE LOS DATOS DEL SERVICIO
    this.respuestasUsuario = this.triviaService.getResultados();
+   //OBTIENE LOS TEMAS QUE SELECCIONO EL USUARIO
    this.temasSeleccionados = this.respuestasUsuario.map(r => r.pregunta.temas);
+   //FILTRA POR NOMBRES UNICOS A LOS TEMAS, PARA MOSTRARLOS EN EL TEMPLATE
    this.nombreTemasSeleccionados = this.temasSeleccionados.map(tema => tema.name)
   .filter((nombre, index, self) => self.indexOf(nombre) === index);
 
+  this.respuestasUsuario.forEach((respuesta) => {
+    // Obtenemos el tema de la pregunta
+    const tema = respuesta.pregunta.temas.name;
+
+    // Si el tema aún no está en el objeto, lo agregamos con un array vacío
+    if (!this.temasPreguntas[tema]) {
+      this.temasPreguntas[tema] = [];
+    }
+
+    // Agregamos la pregunta al array del tema correspondiente
+    this.temasPreguntas[tema].push({ pregunta: respuesta.pregunta, esCorrecta: respuesta.esCorrecta });
+  });
+
+  // Ahora tenemos un objeto con los temas como keys y un array de preguntas como valor
+  console.log(this.temasPreguntas)
 
    this.authenticationService.getCurrentUser().subscribe(user => {
     if (user) {
@@ -34,15 +54,17 @@ export class ResultadosComponent implements OnInit {
       console.log(this.currentUser)
       // Aquí puedes llamar a otro servicio para obtener más información del usuario si lo necesitas
     }
-
   });
    console.log(this.respuestasUsuario);
-   console.log(this.temasSeleccionados);
    console.log(this.nombreTemasSeleccionados)
   }
 
   getRespuestasForTema(tema: Temas) {
     return this.respuestasUsuario.filter(r => r.pregunta.temas.id === tema.id);
+
   }
 
+  objectKeys(temasPreguntas:any) {
+    return  Object.keys(temasPreguntas);
+  }
 }
