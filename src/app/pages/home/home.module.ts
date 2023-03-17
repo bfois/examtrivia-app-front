0f8b5d259c38259b278a2a6081152f8403858775
BraianF <97342723+bfois@users.noreply.github.com>
2023-03-17 10:55:17 -0300

    introduccion elements

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -14,6 +14,7 @@ import {MatSelectModule} from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { TemasComponent } from './temas/temas.component';
 import { FooterComponent } from 'src/app/shared/footer/footer.component';
+import { IntroduccionComponent } from './introduccion/introduccion.component';
 
 //RUTAS
 const routes: Routes = [
@@ -23,6 +24,7 @@ const routes: Routes = [
 @NgModule({
     declarations: [
         HomeComponent,
+        IntroduccionComponent,
     ],
     imports: [
         CommonModule,
