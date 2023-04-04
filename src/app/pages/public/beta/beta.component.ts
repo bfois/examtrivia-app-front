5138e9da3edff5081977d7d05fdee8009cf8d92e
BraianF <97342723+bfois@users.noreply.github.com>
2023-04-04 12:06:23 -0300

    beta styles

--- src/app/pages/public/beta/beta.component.ts
+++ src/app/pages/public/beta/beta.component.ts
@@ -1,18 +1,25 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+const efectStart = transition('void => *', [
+  style({ opacity: 0 }),
+  animate('5s ease-in', style({ opacity: 1 })),
+])
+const animation = trigger('animation',[efectStart])
 
 @Component({
   selector: 'app-beta',
   template: `
   <section class="main">
   <ngx-typed-js
-  [strings]="['¡Si vez este mensaje es porque nos encontramos en nuestra versión beta, actualmente puedes pobrar la aplicación con la disciplina matemática y sus materias: Calculo diferencial e integral, Estadísticas y probabilidad, que te diviertas...']"
+  [strings]="['¡Si vez este mensaje es porque nos encontramos en nuestra versión beta, actualmente puedes pobrar la aplicación con la disciplina matemática y sus materias: Calculo diferencial e integral, Estadísticas y Probabilidad, que te diviertas...']"
   [loop]="false"
   [shuffle]="true" [typeSpeed]="30"
   [startDelay]="100"
   [showCursor]="false"
   >
+  <span class="typing"></span>
   </ngx-typed-js>
-  <button routerLink="signin" mat-button>ENTRAR A LA APP</button>
+  <button [@animation] routerLink="/signin" mat-button>ENTRAR A LA APP</button>
 </section>
   `,
   styles: [`
@@ -25,15 +32,28 @@ import { Component, OnInit } from '@angular/core';
     background-color:black;
     color:white;
     font-size:20px;
+    flex-direction:column;
   }
   button{
+    margin-top:20px;
     background-color:orange;
     font-weight:bold;
-    font-size:20px;
+    font-size:16px;
     padding:5px;
     color:black;
   }
-  `]
+  ngx-typed-js{
+    width:50%;
+    height:10%;
+    text-align:justify;
+  }
+  @media (max-width: 767px){
+    ngx-typed-js{
+      width:90%;
+    }
+  }
+  `],
+  animations:[animation]
 })
 export class BetaComponent implements OnInit {
 
